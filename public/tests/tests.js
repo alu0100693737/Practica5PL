5f618ecfba76fe904f71356fd84f17a586dd83f9
Ivan Garcia <alu0100693737@ull.edu.es>
2014-03-20 22:41:12 +0100

    arreglado tests

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -1,24 +1,25 @@
 
 var assert = chai.assert;
 
-suite('Analizador Descendente Recursivo Predictivo,  Los tests estan mal hechos pero no entiendo el porque, creo que tiene algo que ver con lo esperado en cada una de las funciones',
+suite('Analizador Descendente Recursivo Predictivo',
 function() {
     test('Suma y asignacion: ', function() {
 original.value = "a = 2+3";
-var wait = '[\n {\n "type": "=",\n "left": {\n "type": "ID",\n "value": "a"\n },\n "right": {\n "type": "+",\n "left": {\n "type": "NUM",\n "value": 2\n },\n "right": {\n "type": "NUM",\n "value": 3\n }\n }\n }\n]';
+var wait = '[\n  {\n    "type": "=",\n    "left": {\n      "type": "ID",\n      "value": "a"\n    },\n    "right": {\n      "type": "+",\n      "left": {\n        "type": "NUM",\n        "value": 2\n      },\n      "right": {\n        "type": "NUM",\n        "value": 3\n      }\n    }\n  }\n]' ;
+
 main ();
        assert.deepEqual(OUTPUT.innerHTML, wait);
     });
 
 test('If then: ', function() {
 original.value = "a = 4*2; if a > 6 then p a";
-var wait = '[\n [\n {\n "type": "=",\n "left": {\n "type": "ID",\n "value": "a"\n },\n "right": {\n "type": "*",\n "left": {\n "type": "NUM",\n "value": 4\n },\n "right": {\n "type": "NUM",\n "value": 2\n }\n }\n }\n ],\n {\n "type": "IF",\n "left": {\n "type": "&gt;",\n "left": {\n "type": "ID",\n "value": "a"\n },\n "right": {\n "type": "NUM",\n "value": 6\n }\n },\n "right": {\n "type": "P",\n "value": {\n "type": "ID",\n "value": "a"\n }\n }\n }\n]';
+var wait = '[\n  [\n    {\n      "type": "=",\n      "left": {\n        "type": "ID",\n        "value": "a"\n      },\n      "right": {\n        "type": "*",\n        "left": {\n          "type": "NUM",\n          "value": 4\n        },\n        "right": {\n          "type": "NUM",\n          "value": 2\n        }\n      }\n    }\n  ],\n  {\n    "type": "IF",\n    "left": {\n      "type": "&gt;",\n      "left": {\n        "type": "ID",\n        "value": "a"\n      },\n      "right": {\n        "type": "NUM",\n        "value": 6\n      }\n    },\n    "right": {\n      "type": "P",\n      "value": {\n        "type": "ID",\n        "value": "a"\n      }\n    }\n  }\n]';
 main ();
        assert.deepEqual(OUTPUT.innerHTML, wait);
     });
 test('While do: ', function() {
 original.value = "while a != 1 do b = 4";
-var wait = '[\n {\n "type": "WHILE",\n "left": {\n "type": "!=",\n "left": {\n "type": "ID",\n "value": "a"\n },\n "right": {\n "type": "NUM",\n "value": 1\n }\n },\n "right": {\n "type": "=",\n "left": {\n "type": "ID",\n "value": "b"\n },\n "right": {\n "type": "NUM",\n "value": 4\n }\n }\n }\n]';
+var wait = '[\n  {\n    "type": "WHILE",\n    "left": {\n      "type": "!=",\n      "left": {\n        "type": "ID",\n        "value": "a"\n      },\n      "right": {\n        "type": "NUM",\n        "value": 1\n      }\n    },\n    "right": {\n      "type": "=",\n      "left": {\n        "type": "ID",\n        "value": "b"\n      },\n      "right": {\n        "type": "NUM",\n        "value": 4\n      }\n    }\n  }\n]';
 main ();
        assert.deepEqual(OUTPUT.innerHTML, wait);
     });
