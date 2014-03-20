78714fa813c9e770752ee3e308671ed95ee09b9c
Ivan Garcia <alu0100693737@ull.edu.es>
2014-03-20 01:07:36 +0100

    cambios en las pruebas

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -1,69 +1,30 @@
+
 var assert = chai.assert;
 
-suite('Tokens', function(){
-  // Probar la función bexec
-  test('RegExp.bexec()', function(){
-    var input_str = "prueba";
-var regexp = /ba/;
-regexp.lastIndex = 0;
+suite('Analizador Descendente Recursivo Predictivo,  Los tests estan mal hechos pero no entiendo el porque, creo que tiene algo que ver con lo esperado en cada una de las funciones',
+function() {
+    test('Suma y asignacion: ', function() {
+original.value = "a = 2+3";
+var wait = '[\n {\n "type": "=",\n "left": {\n "type": "ID",\n "value": "a"\n },\n "right": {\n "type": "+",\n "left": {\n "type": "NUM",\n "value": 2\n },\n "right": {\n "type": "NUM",\n "value": 3\n }\n }\n }\n]';
+main ();
+       assert.deepEqual(OUTPUT.innerHTML, wait);
+    });
 
-assert.equal(regexp.bexec(input_str), null);
-  });
-
-  // Probar la función String.tokens sobre una string sencilla.
-  test('String.tokens()', function(){
-    var input_str = "var a = b;";
-    var esperado_str = "[{\"type\":\"ID\",\"value\":\"var\",\"from\":0,\"to\":3},{\"type\":\"ID\",\"value\":\"a\",\"from\":4,\"to\":5},{\"type\":\"=\",\"value\":\"=\",\"from\":6,\"to\":7},{\"type\":\"ID\",\"value\":\"b\",\"from\":8,\"to\":9},{\"type\":\";\",\"value\":\";\",\"from\":9,\"to\":10}]";
-var resultado_str = JSON.stringify(input_str.tokens());
-
-assert.equal(esperado_str, resultado_str);
-  });
-  
-  // error
-  test('String.tokens(): Exccepción de error', function(){
-    var input_str = "#ERROR#";
-var resultado_str = "Syntax error near '#ERROR#'";
-
-    chai.expect(function () { input_str.tokens() }).to.throw(resultado_str);
-  });
-});
-
-suite('Parser', function(){
-  // Probamos el parser.
-  test('Parser', function(){
-var input_str = "VAR x, squ; PROCEDURE square; BEGIN squ = x * x END; BEGIN x = 1; WHILE x <= 10 DO BEGIN CALL square; x = x + 1 END END.";
-var result = window.parse(input_str);
-var esperado_str = "[\n {\n \"type\": \"Var ID\",\n \"value\": \"x\"\n },\n {\n \"type\": \"Var ID\",\n \"value\": \"squ\"\n },\n {\n \"type\": \"Procedure\",\n \"value\": \"square\",\n \"left\": [\n [\n {\n \"type\": \"=\",\n \"left\": {\n \"type\": \"ID\",\n \"value\": \"squ\"\n },\n \"right\": {\n \"type\": \"*\",\n \"left\": {\n \"type\": \"ID\",\n \"value\": \"x\"\n },\n \"right\": {\n \"type\": \"ID\",\n \"value\": \"x\"\n }\n }\n }\n ]\n ]\n },\n [\n {\n \"type\": \"=\",\n \"left\": {\n \"type\": \"ID\",\n \"value\": \"x\"\n },\n \"right\": {\n \"type\": \"NUM\",\n \"value\": 1\n }\n },\n {\n \"type\": \"WHILE\",\n \"left\": {\n \"type\": \"<=\",\n \"left\": {\n \"type\": \"ID\",\n \"value\": \"x\"\n },\n \"right\": {\n \"type\": \"NUM\",\n \"value\": 10\n }\n },\n \"right\": [\n {\n \"type\": \"CALL\",\n \"value\": \"square\"\n },\n {\n \"type\": \"=\",\n \"left\": {\n \"type\": \"ID\",\n \"value\": \"x\"\n },\n \"right\": {\n \"type\": \"+\",\n \"left\": {\n \"type\": \"ID\",\n \"value\": \"x\"\n },\n \"right\": {\n \"type\": \"NUM\",\n \"value\": 1\n }\n }\n }\n ]\n }\n ]\n]";
-
-var resultado_str, tree;
-    try {
-      tree = parse(input_str);
-      resultado_str = JSON.stringify(tree, null, 4);
-    } catch (e) {
-      resultado_str = JSON.stringify(e, null, 4);
-    }
-
-assert.equal(esperado_str, resultado_str);
-  });
-  
-  // Probamos el parser con errores.
-  test('Parser: Errores', function(){
-   var input_str = "error = $;";
-var esperado_str = "Syntax error near '$;'";
-
-    chai.expect(function () { window.parse(input_str) }).to.throw(esperado_str);
-  });
-});
-
-suite('Otros', function(){
-  // Probar CodeMirror+
-  test('CodeMirror', function(){
-    var editor = $('.CodeMirror')[0].CodeMirror;
-    editor.setValue("CONST x = 0; \nVAR a, b, c;\nBEGIN\nIF a == 1 THEN\nWHILE b <= 5 DO\na = a + 1\nEND.");
-    window.main();
-  
-    var esperado_str = "[\n {\n \"type\": \"=\",\n \"left\": {\n \"type\": \"Const ID\",\n \"value\": \"x\"\n },\n \"right\": {\n \"type\": \"NUM\",\n \"value\": 0\n }\n },\n {\n \"type\": \"Var ID\",\n \"value\": \"a\"\n },\n {\n \"type\": \"Var ID\",\n \"value\": \"b\"\n },\n {\n \"type\": \"Var ID\",\n \"value\": \"c\"\n },\n [\n {\n \"type\": \"IF\",\n \"left\": {\n \"type\": \"==\",\n \"left\": {\n \"type\": \"ID\",\n \"value\": \"a\"\n },\n \"right\": {\n \"type\": \"NUM\",\n \"value\": 1\n }\n },\n \"right\": {\n \"type\": \"WHILE\",\n \"left\": {\n \"type\": \"&lt;=\",\n \"left\": {\n \"type\": \"ID\",\n \"value\": \"b\"\n },\n \"right\": {\n \"type\": \"NUM\",\n \"value\": 5\n }\n },\n \"right\": {\n \"type\": \"=\",\n \"left\": {\n \"type\": \"ID\",\n \"value\": \"a\"\n },\n \"right\": {\n \"type\": \"+\",\n \"left\": {\n \"type\": \"ID\",\n \"value\": \"a\"\n },\n \"right\": {\n \"type\": \"NUM\",\n \"value\": 1\n }\n }\n }\n }\n }\n ]\n]";
-    var resultado_str = OUTPUT.innerHTML;
-    assert.equal(esperado_str, resultado_str);
-  });
+test('If then: ', function() {
+original.value = "a = 4*2; if a > 6 then p a";
+var wait = '[\n [\n {\n "type": "=",\n "left": {\n "type": "ID",\n "value": "a"\n },\n "right": {\n "type": "*",\n "left": {\n "type": "NUM",\n "value": 4\n },\n "right": {\n "type": "NUM",\n "value": 2\n }\n }\n }\n ],\n {\n "type": "IF",\n "left": {\n "type": "&gt;",\n "left": {\n "type": "ID",\n "value": "a"\n },\n "right": {\n "type": "NUM",\n "value": 6\n }\n },\n "right": {\n "type": "P",\n "value": {\n "type": "ID",\n "value": "a"\n }\n }\n }\n]';
+main ();
+       assert.deepEqual(OUTPUT.innerHTML, wait);
+    });
+test('While do: ', function() {
+original.value = "while a != 1 do b = 4";
+var wait = '[\n {\n "type": "WHILE",\n "left": {\n "type": "!=",\n "left": {\n "type": "ID",\n "value": "a"\n },\n "right": {\n "type": "NUM",\n "value": 1\n }\n },\n "right": {\n "type": "=",\n "left": {\n "type": "ID",\n "value": "b"\n },\n "right": {\n "type": "NUM",\n "value": 4\n }\n }\n }\n]';
+main ();
+       assert.deepEqual(OUTPUT.innerHTML, wait);
+    });
+test('Error: ', function() {
+original.value = "a = 3 + (4; b = 5";
+main ();
+       assert.match(OUTPUT.innerHTML, /Error/);
+    });
 });
